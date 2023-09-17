9883def258213a80031d139d141e649b79168d2e
Shoaib Shams <shoaibshamshere@gmail.com>
2023-09-17 21:35:00 +0500

    sidemenu fixes for mobile

--- resources/views/components/layouts/sidenav.blade.php
+++ resources/views/components/layouts/sidenav.blade.php
@@ -1,6 +1,73 @@
+<nav class="navbar navbar-dark navbar-theme-primary px-4 col-12 d-lg-none">
+    <a
+        class="navbar-brand me-lg-5"
+        href="{{ route('home') }}">
+        <img
+            class="navbar-brand-dark"
+            src="{{ asset('assets/img/brand/light.svg') }}"
+            alt="Volt logo"> <img class="navbar-brand-light" src="{{ asset('assets/img/brand/dark.svg') }}" alt="Volt logo">
+    </a>
+    <div class="d-flex align-items-center">
+        <button
+            class="navbar-toggler d-lg-none collapsed"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#sidebarMenu"
+            aria-controls="sidebarMenu"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+
 <nav id="sidebarMenu" class="sidebar d-lg-block bg-gray-800 text-white collapse" data-simplebar>
     <div class="sidebar-inner px-2 pt-3">
-
+        <div class="user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4">
+            <div class="d-flex align-items-center">
+                <div class="avatar-lg me-4">
+                    <img
+                        src="{{ asset('images/user.png') }}" class="card-img-top rounded-circle border-white bg-white"
+                        alt="{{ auth()->user()->name }}">
+                </div>
+                <div class="d-block">
+                    <h2 class="h5 mb-3">Hi, {{ auth()->user()->name }}</h2>
+                    <form action="{{ route('logout') }}" method="post">
+                        @csrf
+                        <button type="submit" class="btn btn-secondary btn-sm d-inline-flex align-items-center">
+                            <svg
+                                class="icon icon-xxs me-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path>
+                            </svg>
+                            Sign Out
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <div class="collapse-close d-md-none">
+                <a
+                    href="#sidebarMenu"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#sidebarMenu"
+                    aria-controls="sidebarMenu"
+                    aria-expanded="true"
+                    aria-label="Toggle navigation">
+                    <svg
+                        class="icon icon-xs"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            fill-rule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
         <ul class="nav flex-column pt-3 pt-md-0">
             <li class="nav-item">
                 <a href="{{ route('home') }}" wire:navigate class="nav-link d-flex align-items-center">
